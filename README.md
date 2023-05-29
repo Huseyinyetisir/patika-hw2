a5a3b5ea2818cf48bd5c421378159122a8784cb7
Hüseyin Yetişir <huseynyetisir@gmail.com>
2023-05-29 11:03:31 +0300

    Readme File Edited

---
+++ README.md
@@ -0,0 +1,53 @@
+# Invoice Management Application
+
+This application allows customers to create orders, save their invoices, and view them.
+
+## Requirements
+
+The application should meet the following requirements:
+
+- [x] Final, static, overload, override, constructor, inheritance, polymorphism, encapsulation, List, Set, Map concepts should be used.
+- [x] The system should provide the ability to add new customers.
+- [x] The system should provide the ability to add invoices to customers.
+- [x] The system should list all customers.
+- [x] The system should list customers whose names contain the letter 'C'.
+- [x] The system should calculate and display the total invoice amount for customers registered in the month of June.
+- [x] The system should list all invoices in the system.
+- [x] The system should list invoices with an amount above 1500TL.
+- [x] The system should calculate and display the average amount of invoices above 1500TL.
+- [x] The system should list the names of customers with invoices below 500TL.
+- [x] The system should list the sectors in which the average invoice amount for customers registered in June is below 750.
+
+## Usage
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/invoice-management.git
+
+2. Build the project using Maven:
+    ```bash
+   cd invoice-management
+   mvn clean install
+
+3. Run the application:
+    ```bash
+    java -jar target/invoice-management.jar
+
+Follow the on-screen prompts to perform various operations:
+
+- Choose option 1 to create a new customer and add invoices.
+- Choose option 2 to list all customers.
+- Choose option 3 to list customers with 'C' in their names.
+- Choose option 4 to calculate the total invoice amount for customers registered in June.
+- Choose option 5 to list all invoices in the system.
+- Choose option 6 to list invoices with amounts above 1500TL.
+- Choose option 7 to calculate the average amount of high-value invoices.
+- Choose option 8 to list customer names with invoices below 500TL.
+- Choose option 9 to list sectors with average invoice amounts below 750TL for customers registered in June.
+
+
+### Please feel free to contribute to this project by submitting bug reports, feature requests, or pull requests.
+
+License
+This project is licensed under the MIT License. See the LICENSE file for more information.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,148 @@
 package org.example;
 
+import org.example.model.Customer;
+import org.example.model.Invoice;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Customer> customers = createCustomers();
+
+        listAllCustomers(customers);
+
+        listCustomersWithC(customers);
+
+        calculateTotalInvoiceAmountForJuneCustomers(customers);
+
+        listAllInvoices(customers);
+
+        listHighAmountInvoices(customers);
+
+        calculateAverageAmountOfHighInvoices(customers);
+
+        listCustomerNamesWithLowAmountInvoices(customers);
+
+        listSectorsWithLowAverageAmountInJuneInvoices(customers);
+    }
+
+    // List all customers
+    public static void listAllCustomers(List<Customer> customers) {
+        System.out.println("All customers:");
+        customers.forEach(System.out::println);
+    }
+
+    // List customers with 'C' in their names
+    public static void listCustomersWithC(List<Customer> customers) {
+        System.out.println("\nCustomers with 'C' in their names:");
+        List<Customer> customersWithC = customers.stream()
+                .filter(c -> c.getName().contains("C"))
+                .collect(Collectors.toList());
+        customersWithC.forEach(System.out::println);
+    }
+
+    // Calculate total invoice amount for customers registered in June
+    public static void calculateTotalInvoiceAmountForJuneCustomers(List<Customer> customers) {
+        System.out.println("\nTotal invoice amounts for customers registered in June:");
+        customers.stream()
+                .filter(c -> c.getRegistrationDate().getMonth() == Month.JUNE)
+                .forEach(c -> {
+                    double totalAmount = c.getInvoices().stream()
+                            .mapToDouble(Invoice::getAmount)
+                            .sum();
+                    System.out.println(c.getName() + ": " + totalAmount);
+                });
+    }
+
+    // List all invoices in the system
+    public static void listAllInvoices(List<Customer> customers) {
+        System.out.println("\nAll invoices:");
+        customers.stream()
+                .flatMap(c -> c.getInvoices().stream())
+                .forEach(System.out::println);
+    }
+
+    // List invoices with amount above 1500TL
+    public static void listHighAmountInvoices(List<Customer> customers) {
+        System.out.println("\nInvoices with amount above 1500TL:");
+        List<Invoice> highAmountInvoices = customers.stream()
+                .flatMap(c -> c.getInvoices().stream())
+                .filter(i -> i.getAmount() > 1500)
+                .collect(Collectors.toList());
+        highAmountInvoices.forEach(System.out::println);
+    }
+
+    // Calculate average amount of invoices above 1500TL
+    public static void calculateAverageAmountOfHighInvoices(List<Customer> customers) {
+        System.out.println("\nAverage amount of invoices above 1500TL:");
+        double averageAmount = customers.stream()
+                .flatMap(c -> c.getInvoices().stream())
+                .filter(i -> i.getAmount() > 1500)
+                .mapToDouble(Invoice::getAmount)
+                .average()
+                .orElse(0.0);
+        System.out.println(averageAmount);
+    }
+
+    // List customer names with invoices below 500TL
+    public static void listCustomerNamesWithLowAmountInvoices(List<Customer> customers) {
+        System.out.println("\nCustomer names with invoices below 500TL:");
+        List<String> customerNames = customers.stream()
+                .filter(c -> c.getInvoices().stream().anyMatch(i -> i.getAmount() < 500))
+                .map(Customer::getName)
+                .collect(Collectors.toList());
+        customerNames.forEach(System.out::println);
+    }
+
+    // List sectors with average invoice amount below 750TL for customers registered in June
+    public static void listSectorsWithLowAverageAmountInJuneInvoices(List<Customer> customers) {
+        System.out.println("\nSectors with average invoice amount below 750TL for June invoices:");
+
+        customers.stream()
+                .filter(c -> c.getRegistrationDate().getMonth() == Month.JUNE)
+                .flatMap(c -> c.getInvoices().stream())
+                .collect(Collectors.groupingBy(Invoice::getSector,
+                        Collectors.averagingDouble(Invoice::getAmount)))
+                .forEach((sector, averageAmount) -> {
+                    if (averageAmount < 750) {
+                        System.out.println("Sector: " + sector);
+                    }
+                });
+    }
+
+    // Helper method to create sample customers
+    public static List<Customer> createCustomers() {
+        List<Customer> customers = new ArrayList<>();
+
+        // Customer 1
+        Customer customer1 = new Customer("Cem", LocalDate.of(2022, Month.JANUARY, 1));
+        Invoice invoice11 = new Invoice("Invoice 1", 1000, "IT");
+        Invoice invoice12 = new Invoice("Invoice 2", 2000, "IT");
+        customer1.addInvoice(invoice11);
+        customer1.addInvoice(invoice12);
+        customers.add(customer1);
+
+        // Customer 2
+        Customer customer2 = new Customer("Customer 2", LocalDate.of(2022, Month.JUNE, 15));
+        Invoice invoice21 = new Invoice("Invoice 3", 500, "Finance");
+        Invoice invoice22 = new Invoice("Invoice 4", 100, "Finance");
+        customer2.addInvoice(invoice21);
+        customer2.addInvoice(invoice22);
+        customers.add(customer2);
+
+        // Customer 3
+        Customer customer3 = new Customer("Customer 3", LocalDate.of(2023, Month.MARCH, 10));
+        Invoice invoice31 = new Invoice("Invoice 5", 500, "Estate");
+        Invoice invoice32 = new Invoice("Invoice 6", 200, "Estate");
+        customer3.addInvoice(invoice31);
+        customer3.addInvoice(invoice32);
+        customers.add(customer3);
+
+        return customers;
     }
 }
